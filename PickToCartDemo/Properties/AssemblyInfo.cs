e6e1d43671653bbfef217756ade8102e9d800c3f
Eric Chong <echong@microsoft.com>
2017-09-08 09:48:50 -0700

    Removed the dependancy of the cert

--- PickToCartDemo/Properties/AssemblyInfo.cs
+++ PickToCartDemo/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Windows.Media;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PickTheCartDemo")]
+[assembly: AssemblyTitle("PickToCartDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PickTheCartDemo")]
+[assembly: AssemblyCompany("Watchtower")]
+[assembly: AssemblyProduct("PickToCartDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
